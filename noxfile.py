9fba25def1102ebef3d565963eb43204308698e1
harshil93r <harshil9968@gmail.com>
2020-12-18 15:30:17 +0530

    removed js folder and nox

--- noxfile.py
+++
@@ -1,9 +0,0 @@
-import nox
-from nox import session
-
-@nox.session(python="3.7")
-def coverage(session: session) -> None:
-    """Upload coverage data."""
-    # install_with_constraints(session, "coverage[toml]", "codecov")
-    session.run("coverage", "xml", "--fail-under=0")
-    session.run("codecov", *session.posargs)
