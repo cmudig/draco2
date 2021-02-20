8d3fac0cce9442287aead310e93b0c5b8604d0aa
Shreya <shreyab261@gmail.com>
2021-02-19 19:28:55 -0500

    Before latest fetch

--- draco/fact_utils.py
+++ draco/fact_utils.py
@@ -1,6 +1,6 @@
 from collections import abc
 from enum import Enum, unique
-from typing import Generator, List, Mapping
+from typing import Any, Dict, Generator, List, Mapping, Union
 
 
 @unique
@@ -61,9 +61,92 @@ def dict_to_facts(
             )
 
 
+def extract_elems(fact: str):
+    """
+    Splits every fact into a tuple of:
+    (property or attribute, list of comma separated values)
+    """
+    kind = fact[: fact.index("(")]
+    str_values = fact[fact.index("(") + 1 : fact.index(")")]
+    list_values = str_values.split(",")
+    if kind == FactKind.PROPERTY.value:
+        return (FactKind.PROPERTY, list_values)
+    else:
+        return (FactKind.ATTRIBUTE, list_values)
+
+
+def deep_nest(Root: Mapping, current_dict: Mapping):
+    """
+    Indexes into the memo dictionary and creates the nested data structure
+    """
+    for key in current_dict:
+        if isinstance(current_dict[key], list):
+            current_list = current_dict[key]
+            for i in range(len(current_list)):
+                assert current_list[i] in Root
+                index = current_list[i]
+                current_list[i] = Root[index]
+                deep_nest(Root, current_list[i])
+
+
+def remove_memo(Root: Dict[Union[str, int], Any]):
+    """
+    Removes the integer ID mappings in the dictionary
+    """
+    to_delete = []
+    for key in Root:
+        if isinstance(key, int):
+            to_delete.append(key)
+
+    for key in to_delete:
+        del Root[key]
+
+
+def parse_value(val: str):
+    """
+    If the value is a number, the function converts the string to a nuumber
+    and return the string otherwise
+    """
+    try:
+        Numeric = float(val)
+        return Numeric
+    except ValueError:
+        return val
+
+
 def facts_to_dict(facts: List) -> Mapping:
     """
     A generic decoder that converts an answer set into a nested data structure
+
     """
-    pass  # TODO: https://github.com/cmudig/draco2/issues/24
-    return {}
+    Root: Dict[Union[int, str], Any] = dict()
+    # Creating the memo dictionary that maps numbers to nested properties
+    if facts == []:
+        return Root
+    for fact in facts:
+        kind, values = extract_elems(fact)
+        (key, parent_index, val) = values
+        if kind.value == FactKind.PROPERTY.value:
+            if parent_index == "root":
+                if key in Root:
+                    Root[key].append(int(val))
+                else:
+                    Root[key] = [int(val)]
+                Root[int(val)] = dict()
+            else:
+                if key in Root[int(parent_index)]:
+                    Root[int(parent_index)][key].append(int(val))
+                else:
+                    Root[int(parent_index)][key] = [int(val)]
+                Root[int(val)] = dict()
+        else:
+            if parent_index == "root":
+                Root[key] = parse_value(val)
+            else:
+                Root[int(parent_index)][key] = parse_value(val)
+    # Running through the memoized dictionary to replace the integer ID
+    # with the actual property
+    deep_nest(Root, Root)
+    # Removing integer ID -> property mappings from the dictionary
+    remove_memo(Root)
+    return Root
