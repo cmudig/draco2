53468af384135a98a4f8b6d052c957d6ed48c934
JunranY <35775163+JunranY@users.noreply.github.com>
2022-01-20 10:55:53 -0800

    feat: port some hard constraints from Draco 1 (#183)

--- draco/tests/test_hard.py
+++ draco/tests/test_hard.py
@@ -86,3 +86,231 @@ def test_text_channel_without_text_mark():
     :- violation(_).
     """
     )
+
+
+def test_bin_and_aggregate():
+    b = hard.blocks["bin_and_aggregate"]
+    assert isinstance(b, Block)
+    p = b.program
+
+    assert is_satisfiable(
+        p
+        + """
+    entity(encoding,root,e1).
+    attribute((encoding,channel),e1,x).
+    attribute((encoding,binning),e1,10).
+    entity(encoding,root,e2).
+    attribute((encoding,channel),e2,y).
+    attribute((encoding,aggregate),e2,count).
+
+    :- violation(_).
+    """
+    )
+
+    assert not is_satisfiable(
+        p
+        + """
+    entity(encoding,root,e1).
+    attribute((encoding,channel),e1,x).
+    attribute((encoding,binning),e1,10).
+    attribute((encoding,aggregate),e1,count).
+
+    :- violation(_).
+    """
+    )
+
+
+def test_no_encodings():
+    b = hard.blocks["no_encodings"]
+    assert isinstance(b, Block)
+    p = b.program
+
+    assert is_satisfiable(
+        p
+        + """
+    entity(mark,root,1).
+    entity(encoding,1,2).
+
+    :- violation(_).
+    """
+    )
+
+    assert not is_satisfiable(
+        p
+        + """
+    entity(mark,root,1).
+    entity(encoding,1,2).
+    entity(mark,root,3).
+
+    :- violation(_).
+    """
+    )
+
+
+def test_repeat_channel():
+    b = hard.blocks["repeat_channel"]
+    assert isinstance(b, Block)
+    p = helpers.program + b.program
+
+    assert is_satisfiable(
+        p
+        + """
+    attribute((encoding,channel),e0,x).
+    attribute((encoding,channel),e1,y).
+
+
+    :- violation(_).
+    """
+    )
+
+    assert is_satisfiable(
+        p
+        + """
+    entity(encoding,1,2).
+    attribute((encoding,channel),2,x).
+    attribute((encoding,field),2,temperature).
+    entity(scale,root,3).
+    attribute((scale,channel),3,x).
+
+    :- violation(_).
+    """
+    )
+
+    assert is_satisfiable(
+        p
+        + """
+    entity(encoding,1,2).
+    attribute((encoding,channel),2,x).
+    attribute((encoding,field),2,temperature).
+    entity(encoding,1,3).
+    attribute((encoding,channel),3,x).
+
+    :- violation(_).
+    """
+    )
+
+    assert is_satisfiable(
+        p
+        + """
+    entity(mark,root,1).
+
+    entity(encoding,1,2).
+    attribute((encoding,channel),2,x).
+    attribute((encoding,field),2,temperature).
+
+    entity(encoding,1,3).
+    attribute((encoding,channel),3,y).
+
+    :- violation(_).
+    """
+    )
+
+    assert not is_satisfiable(
+        p
+        + """
+    entity(mark,root,1).
+
+    entity(encoding,1,2).
+    attribute((encoding,channel),2,x).
+    attribute((encoding,field),2,temperature).
+
+    entity(encoding,1,3).
+    attribute((encoding,channel),3,x).
+
+    :- violation(_).
+    """
+    )
+
+
+def test_encoding_no_field_and_not_count():
+    b = hard.blocks["encoding_no_field_and_not_count"]
+    assert isinstance(b, Block)
+    p = b.program
+    assert is_satisfiable(
+        p
+        + """
+    entity(encoding,root,1).
+    attribute((encoding,field),1,temp).
+    attribute((encoding,aggregate),1,mean).
+
+
+    :- violation(_).
+    """
+    )
+
+    assert is_satisfiable(
+        p
+        + """
+    entity(encoding,root,1).
+    attribute((encoding,aggregate),1,count).
+
+
+    :- violation(_).
+    """
+    )
+
+    assert not is_satisfiable(
+        p
+        + """
+    entity(encoding,root,1).
+    attribute((encoding,aggregate),1,mean).
+
+
+    :- violation(_).
+    """
+    )
+
+
+def test_count_with_field():
+    b = hard.blocks["count_with_field"]
+    assert isinstance(b, Block)
+    p = b.program
+
+    assert is_satisfiable(
+        p
+        + """
+    entity(encoding,root,1).
+    attribute((encoding,aggregate),1,count).
+
+
+    :- violation(_).
+    """
+    )
+
+    assert not is_satisfiable(
+        p
+        + """
+    attribute((encoding,aggregate),1,count).
+    attribute((encoding,field),1,temp).
+
+    :- violation(_).
+    """
+    )
+
+
+def test_point_tick_bar_without_x_or_y():
+    b = hard.blocks["point_tick_bar_without_x_or_y"]
+    assert isinstance(b, Block)
+    p = b.program
+
+    assert is_satisfiable(
+        p
+        + """
+    entity(encoding,root,1).
+    attribute((mark,type),1,tick).
+    attribute((encoding,channel),1,x).
+
+
+    :- violation(_).
+    """
+    )
+
+    assert not is_satisfiable(
+        p
+        + """
+    entity(encoding,root,1).
+    attribute((mark,type),1,tick).
+
+    :- violation(_).
+    """
+    )
