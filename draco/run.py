7a3a07b9e77e75180e5ee8ceb32587cfca1ab85a
Zehua Zeng <fiona.zehua.zeng@gmail.com>
2022-03-01 17:27:23 -0500

    docs: document topK argument (#234)

--- draco/run.py
+++ draco/run.py
@@ -1,6 +1,6 @@
 import logging
 from dataclasses import dataclass
-from typing import Any, cast, Generator, Iterable, List, Sequence, Tuple, Union
+from typing import Any, Generator, Iterable, List, Sequence, Tuple, Union, cast
 
 # Clingo Python API is documented at https://potassco.org/clingo/python-api/current/
 import clingo
@@ -37,7 +37,9 @@ def run_clingo(
 
     :param program: Program as a string or iterable of strings that will be
         concatenated.
-    :param models: Number of models to generate, defaults to 0 (meaning all models)
+    :param models: Number of models to generate, defaults to 0 (meaning all models).
+    :param topK: Whether to return the top K models. If false (default), the program
+        will not optimize the output models.
     :yield: The models.
     """
     if not isinstance(program, str):
