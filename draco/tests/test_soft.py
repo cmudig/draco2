86f8fe708cf06046fcd5b435bf799a519680a20b
JunranY <junran@cs.washington.edu>
2022-03-31 10:35:29 -0700

    remove no_preference and only use list_preferences

--- draco/tests/test_soft.py
+++ draco/tests/test_soft.py
@@ -1,6 +1,6 @@
 from draco.asp_utils import Block
 from draco.programs import define, helpers, soft
-from draco.run import is_satisfiable, run_clingo
+from draco.run import run_clingo
 
 
 def list_preferences(program: str):
@@ -16,12 +16,6 @@ def list_preferences(program: str):
         return None
 
 
-def no_preferences(program: str):
-    return is_satisfiable(
-        helpers.program + define.program + program + ":- preference(_)."
-    )
-
-
 def test_list_preferences():
     assert list_preferences(":- a. :- not a.") is None
 
@@ -30,13 +24,16 @@ def test_aggregate():
     b = soft.blocks["aggregate"]
     assert isinstance(b, Block)
 
-    assert no_preferences(
-        b.program
-        + """
+    assert (
+        list_preferences(
+            b.program
+            + """
     attribute((mark,type),m1,text).
     entity(encoding,m1,e1).
     attribute((encoding,channel),e1,text).
     """
+        )
+        == []
     )
 
     assert (
