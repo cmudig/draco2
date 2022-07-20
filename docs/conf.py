449690fd5eac2dc826f979d6570c81458e0cbf14
Péter Ferenc Gyarmati <dev.petergy@gmail.com>
2022-07-20 10:26:50 +0200

    chore: re-generate sphinx config using `jupyter-book config`

--- docs/conf.py
+++ docs/conf.py
@@ -4,33 +4,33 @@
 # re-generate this one.
 ###############################################################################
 author = 'CMU Data Interaction Group'
+autodoc_preserve_defaults = True
 bibtex_bibfiles = ['references.bib']
 comments_config = {'hypothesis': False, 'utterances': False}
-copyright = '2021'
-exclude_patterns = ['**.ipynb_checkpoints', '.DS_Store', 'Thumbs.db', '_build']
+copyright = '2022'
+exclude_patterns = ['logo-dark.png', 'bibliography.md', 'references.bib', 'conf.py', 'intro.md', '__pycache__', '_toc.yml', 'guide.ipynb', 'applications', 'api', 'facts', 'logo-light.png', 'applications/recommendation.ipynb', 'applications/draco_learn.ipynb', 'applications/data', 'applications/data/kim2018_draco2.json', 'api/run.ipynb', 'api/draco.ipynb', 'api/programs.md', 'api/facts.ipynb', 'api/intro.md', 'api/schema.ipynb', 'facts/tick.png', 'facts/hist.png', 'facts/tick_log.png', 'facts/stack_normalize.png', 'facts/view.md', 'facts/bubble.png', 'facts/scale.md', 'facts/facet_bin.png', 'facts/schema.md', 'facts/intro.md', 'facts/binned_hist.png', 'facts/facet.md', 'facts/encoding.md', 'facts/scatter_color.png', 'facts/examples.ipynb', 'facts/mark.md', 'facts/stack.png', 'facts/facet.png', 'facts/task.md', 'facts/scatter.png', 'facts/bar.png', '_build', 'Thumbs.db', '.DS_Store', '**.ipynb_checkpoints']
 execution_allow_errors = False
 execution_excludepatterns = []
 execution_in_temp = False
 execution_timeout = 30
-extensions = ['sphinx_togglebutton', 'sphinx_copybutton', 'myst_nb', 'jupyter_book', 'sphinx_thebe', 'sphinx_comments', 'sphinx_external_toc', 'sphinx.ext.intersphinx', 'sphinx_panels', 'sphinx_book_theme', 'sphinx.ext.autodoc', 'sphinx_autodoc_typehints', 'sphinxcontrib.bibtex', 'sphinx_jupyterbook_latex']
+extensions = ['sphinx_togglebutton', 'sphinx_copybutton', 'myst_nb', 'jupyter_book', 'sphinx_thebe', 'sphinx_comments', 'sphinx_external_toc', 'sphinx.ext.intersphinx', 'sphinx_design', 'sphinx_book_theme', 'sphinx.ext.autodoc', 'sphinx_autodoc_typehints', 'sphinxcontrib.bibtex', 'sphinx_jupyterbook_latex']
 external_toc_exclude_missing = False
-external_toc_path = '_toc.yml'
 html_baseurl = ''
 html_favicon = ''
 html_logo = 'logo-light.png'
 html_sourcelink_suffix = ''
 html_theme = 'sphinx_book_theme'
-html_theme_options = {'search_bar_text': 'Search this book...', 'launch_buttons': {'notebook_interface': 'jupyterlab', 'binderhub_url': 'https://mybinder.org', 'jupyterhub_url': '', 'thebe': False, 'colab_url': ''}, 'path_to_docs': 'docs', 'repository_url': 'https://github.com/cmudig/draco2', 'repository_branch': 'main', 'google_analytics_id': '', 'extra_navbar': 'Powered by <a href="https://jupyterbook.org">Jupyter Book</a>', 'extra_footer': '', 'home_page_in_toc': True, 'use_repository_button': True, 'use_edit_page_button': True, 'use_issues_button': True}
+html_theme_options = {'search_bar_text': 'Search this book...', 'launch_buttons': {'notebook_interface': 'jupyterlab', 'binderhub_url': 'https://mybinder.org', 'jupyterhub_url': '', 'thebe': False, 'colab_url': ''}, 'path_to_docs': 'docs', 'repository_url': 'https://github.com/cmudig/draco2', 'repository_branch': 'main', 'google_analytics_id': '', 'extra_navbar': 'Powered by <a href="https://jupyterbook.org">Jupyter Book</a>', 'extra_footer': '', 'home_page_in_toc': True, 'announcement': '', 'use_repository_button': True, 'use_edit_page_button': True, 'use_issues_button': True, 'single_page': True}
 html_title = 'Draco'
 jupyter_cache = ''
 jupyter_execute_notebooks = 'auto'
 language = None
 latex_engine = 'pdflatex'
+master_doc = '_config'
 myst_enable_extensions = ['linkify', 'substitution']
 myst_url_schemes = ['mailto', 'http', 'https']
 nb_output_stderr = 'show'
 numfig = True
-panels_add_bootstrap_css = False
 pygments_style = 'sphinx'
 suppress_warnings = ['myst.domains']
 use_jupyterbook_latex = True
