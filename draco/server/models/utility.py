5bdaf67f6ac14b2aa0d115e6c94315bf838d585b
Péter Ferenc Gyarmati <dev.petergy@gmail.com>
2022-10-08 17:44:37 +0200

    feat: finalize utility service

--- draco/server/models/utility.py
+++ draco/server/models/utility.py
@@ -2,8 +2,6 @@ from typing import Mapping
 
 from pydantic import BaseModel
 
-from draco.fact_utils import ROOT
-
 from .shared import ClingoSymbol
 
 
@@ -11,8 +9,6 @@ class DictToFactsDTO(BaseModel):  # pytype: disable=base-class-error
     """Data Transfer Object to encode dictionaries as answer set programming facts"""
 
     data: Mapping | list | str
-    path: list = []
-    parent: str | int = ROOT
 
 
 DictToFactsReturn = list[str]
@@ -21,8 +17,7 @@ DictToFactsReturn = list[str]
 class AnswerSetToDictDTO(BaseModel):  # pytype: disable=base-class-error
     """Data Transfer Object to decode an answer set into a nested data structure"""
 
-    answer_set: list[ClingoSymbol]
-    root: str | int = ROOT
+    answer_set: list[ClingoSymbol] | list[str]
 
 
 AnswerSetToDictReturn = Mapping
--- draco/server/routers/utility.py
+++ draco/server/routers/utility.py
@@ -14,10 +14,10 @@ class UtilityRouter(BaseDracoRouter):
         def dict_to_facts(
             dto: endpoint_models.DictToFactsDTO,
         ) -> endpoint_models.DictToFactsReturn:
-            return service.dict_to_facts(dto.data, dto.path, dto.parent)
+            return service.dict_to_facts(dto.data)
 
         @router.post("/answer-set-to-dict")
         def answer_set_to_dict(
             dto: endpoint_models.AnswerSetToDictDTO,
         ) -> endpoint_models.AnswerSetToDictReturn:
-            return service.answer_set_to_dict(dto.answer_set, dto.root)
+            return service.answer_set_to_dict(dto.answer_set)
