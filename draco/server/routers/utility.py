79fe89dbdedf4c8f3e1fe84ce80a4e4dec8e7caa
Péter Ferenc Gyarmati <dev.petergy@gmail.com>
2022-10-28 19:57:12 +0200

    refactor: enhance router DX
    Set default `prefix` and `tags` values

--- draco/server/routers/utility.py
+++ draco/server/routers/utility.py
@@ -5,8 +5,19 @@ from .base import BaseDracoRouter
 
 
 class UtilityRouter(BaseDracoRouter):
-    def __init__(self, draco, *args, **kwargs):
-        super().__init__(draco, *args, **kwargs)
+    """Router exposing utility functionality through REST endpoints."""
+
+    __DEFAULT_PREFIX__ = "/utility"
+    __DEFAULT_TAGS__ = ["Utility"]
+
+    def __init__(self, draco, **kwargs):
+        c = UtilityRouter
+        super().__init__(
+            draco,
+            prefix=kwargs.pop("prefix", c.__DEFAULT_PREFIX__),
+            tags=kwargs.pop("tags", c.__DEFAULT_TAGS__),
+            **kwargs
+        )
 
     @staticmethod
     def _register(router: "BaseDracoRouter") -> None:
