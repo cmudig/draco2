932ed30f8e2825399fccc26f8809341641fed075
Péter Ferenc Gyarmati <dev.petergy@gmail.com>
2022-11-08 08:58:56 +0100

    feat: base renderer progress

--- draco/utils.py
+++ draco/utils.py
@@ -3,3 +3,9 @@ from itertools import chain
 
 def dict_union(*args: dict):
     return dict(chain.from_iterable(d.items() for d in args))
+
+
+def dict_value_by_path(d: dict, path: tuple):
+    for key in path:
+        d = d[key]
+    return d
