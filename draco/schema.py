4c29c2305a20c8d3dab9c0498d7383c4ae6f9d1d
Péter Ferenc Gyarmati <dev.petergy@gmail.com>
2025-07-08 10:07:39 +0200

    feat: use `narwhals` for data schema inference

--- draco/schema.py
+++ draco/schema.py
@@ -1,6 +1,6 @@
 from math import e
 from pathlib import Path
-from typing import Callable, Literal, TypeAlias, TypedDict
+from typing import Callable, Literal, TypeAlias, TypedDict, cast
 
 import narwhals as nw
 import numpy as np
@@ -127,9 +127,9 @@ def schema_from_file(file_path: Path, parse_data_type=dtype_to_field_type) -> Sc
     """
     if file_path.suffix == ".json":
         df = pd.read_json(str(file_path))
-        return schema_from_dataframe(df, parse_data_type)
+        return schema_from_dataframe(cast(IntoDataFrame, df), parse_data_type)
     elif file_path.suffix == ".csv":
         df = pd.read_csv(file_path)
-        return schema_from_dataframe(df, parse_data_type)
+        return schema_from_dataframe(cast(IntoDataFrame, df), parse_data_type)
     else:
         raise ValueError(f"unsupported file type {file_path}")
